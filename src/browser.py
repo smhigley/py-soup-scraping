2a267929f3dd188b998c42a5d4fc3b8876cf32d1
Sarah Higley <sarah.higley@microsoft.com>
2025-10-11 11:57:12 -0700

    update to check if dupe ids are used

--- src/browser.py
+++ src/browser.py
@@ -2,10 +2,10 @@ from playwright.sync_api import sync_playwright
 
 def get_page_content(url):
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)
+        browser = playwright.chromium.launch(headless=False)
         context = browser.new_context(viewport = { "width": 1920, "height": 1080 })
         page = context.new_page()
         page.goto(url)
         page.wait_for_load_state(state="domcontentloaded", timeout=10000)
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(3000)
         return page.content()
--- src/scraper.py
+++ src/scraper.py
@@ -2,10 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from browser import get_page_content
 
-URL = "https://new.kasumata.ee/"
-# page = requests.get(URL)
-# print(page.content)
-# soup = BeautifulSoup(page.content, "html.parser")
+URL = "https://cfapi.centminmod.com/"
 
 # list of attributes that use idrefs for accessibility-related relationships
 attributes = ["for", "aria-activedescendant", "aria-controls", "aria-describedby", "aria-details", "aria-errormessage", "aria-labelledby", "aria-owns"]
@@ -26,30 +23,36 @@ def check_id(id_string):
             return False
     return True
 
-def check_duplicate_ids(new_id):
-    if new_id in ids:
-        duplicate_id_count += 1
-    else:
-        ids.append(new_id)
-
 attribute_count = 0
 invalid_count = 0
-duplicate_id_count = 0
+id_count = 0
 ids = []
+duplicate_ids = []
+duplicate_id_used = 0
 
 page = get_page_content(URL)
 soup = BeautifulSoup(page, "html.parser")
 
+els_with_ids = soup.find_all(id=True)
+for el in els_with_ids:
+    id_count += 1
+    id = el['id']
+    if id in ids:
+        duplicate_ids.append(id)
+    else:
+        ids.append(id)
+
 # Find all elements with an href
 matches = soup.find_all(element_query)
 for element in matches:
-    if element.has_attr("id"):
-        check_duplicate_ids(element["id"])
     for attribute in attributes:
         if attribute in element.attrs:
             attribute_count += 1
             print(f'{attribute} is valid: {check_id(element[attribute])}')
             if not check_id(element[attribute]):
                 invalid_count += 1
+            if element[attribute] in duplicate_ids:
+                duplicate_id_used += 1
+                print(f'  {element[attribute]} uses a duplicate id')
 
-print(f'Found {attribute_count} attributes, {invalid_count} invalid, {duplicate_id_count} duplicate ids')
+print(f'Found {attribute_count} attributes, {invalid_count} invalid, {len(duplicate_ids)} duplicate ids, {id_count} total ids, {duplicate_id_used} duplicate ids used')
